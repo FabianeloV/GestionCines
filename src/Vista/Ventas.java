e3adbe0deb0e6b5ae171e82fbed7db88f5c6da66
FabianeloV <fevr.developer@gmail.com>
2025-06-07 16:25:21 -0500

    dede

---
+++ src/Vista/Ventas.java
@@ -0,0 +1,11 @@
+package Vista;
+
+import javax.swing.*;
+
+public class Ventas {
+    private JPanel VentasPanel;
+    private JButton RegresarBTN;
+    private JLabel pelicula;
+    private JLabel peliculaTitulo;
+    private JLabel duracion;
+}
