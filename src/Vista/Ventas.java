e0b5a1923f77878370385166f8f8bfb0c163a395
Fabianelo <fevr.developer@gmail.com>
2025-06-07 19:25:39 -0500

    Vista pelicula

--- src/Vista/Ventas.java
+++ src/Vista/Ventas.java
@@ -8,4 +8,21 @@ public class Ventas {
     private JLabel pelicula;
     private JLabel peliculaTitulo;
     private JLabel duracion;
+    private JButton menos;
+    private JButton mas;
+    private JRadioButton fecha1;
+    private JRadioButton a09RadioButton;
+    private JRadioButton a10RadioButton;
+    private JRadioButton a11RadioButton;
+    private JRadioButton a1000RadioButton;
+    private JRadioButton a1200RadioButton;
+    private JRadioButton a1400RadioButton;
+    private JRadioButton a1600RadioButton;
+    private JButton escogerAsientoSButton;
+    private JLabel boletos;
+    private JRadioButton a12RadioButton;
+    private JRadioButton a13RadioButton;
+    private JRadioButton a14RadioButton;
+    private JRadioButton a15RadioButton;
+    private JComboBox comboBox1;
 }
