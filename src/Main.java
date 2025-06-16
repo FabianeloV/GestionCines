a573207b075738c6bbe70b2759d743707a9c6387
Pablo <pablo.jaiba2004@gmail.com>
2025-06-16 10:03:16 -0500

    16 jun

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import Vista.Cartelera;
+import Vista.GestionUsuarios;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Cartelera cartelera = new Cartelera();
+        GestionUsuarios gestionUsuarios = new GestionUsuarios();
     }
 }
