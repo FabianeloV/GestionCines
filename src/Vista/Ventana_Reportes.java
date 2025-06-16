0f4a88972b4d3523f605918e4b539fe08e4e928a
katyy <katyyumbla@gmail.com>
2025-06-16 09:55:13 -0500

    vistas

---
+++ src/Vista/Ventana_Reportes.java
@@ -0,0 +1,13 @@
+package Vista;
+
+import javax.swing.*;
+
+public class Ventana_Reportes {
+    private JRadioButton PDFRadioButton;
+    private JRadioButton exelRadioButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JButton aceptarButton;
+    private JButton cancelarButton;
+}
