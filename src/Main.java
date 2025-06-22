42b3e69ed74e136e8b5571df707e5cf6950913b2
Pablo <pablo.jaiba2004@gmail.com>
2025-06-22 18:03:32 -0500

    22 jun-Cartelera Interactiva Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import Controlador.CarteleraControlador;
 import Vista.Cartelera;
+import Vista.EditorPeliculas;
 import Vista.GestionUsuarios;
 
 public class Main {
     public static void main(String[] args) {
-        Cartelera cartelera = new Cartelera();
-        GestionUsuarios gestionUsuarios = new GestionUsuarios();
+
     }
 }
