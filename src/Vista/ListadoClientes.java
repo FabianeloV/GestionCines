79807f62730e7db6de8b343c5f0009387227685a
Fabianelo <fevr.developer@gmail.com>
2025-06-22 10:54:18 -0500

    ListadoClientes

---
+++ src/Vista/ListadoClientes.java
@@ -0,0 +1,12 @@
+package Vista;
+
+import javax.swing.*;
+
+public class ListadoClientes {
+    private JButton filtrarButton;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JButton retrocederButton;
+    private JButton avanzarButton;
+}
