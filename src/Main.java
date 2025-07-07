77f519866237629a7dc55523be2b1ee0419a8897
Fabianelo <fevr.developer@gmail.com>
2025-07-07 12:07:22 -0500

    Fixing 7 julio

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,5 @@ import Vista.GestionUsuarios;
 
 public class Main {
     public static void main(String[] args) {
-        ListadoClientesControlador listadoClientesControlador = new ListadoClientesControlador();
     }
 }
